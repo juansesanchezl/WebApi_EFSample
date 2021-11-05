752468ab6a9347b335888aaf3c5b7a66e2c019af
jsanchez <jsanchez@vida18.com>
2021-11-04 20:05:47 -0500

    First v

---
+++ WebApi_EF/WebApi_EF/Controllers/StatusController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApi_EF.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatusController : ControllerBase
+    {
+        // GET: api/<StatusController>
+        [HttpGet]
+        public string Get()
+        {
+            return "API RUNNING ...";
+        }
+
+        // GET api/<StatusController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<StatusController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<StatusController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<StatusController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebApi_EF/WebApi_EF/Data/WebApi_EFContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApi_EF.Models;
+
+namespace WebApi_EF.Data
+{
+    public class WebApi_EFContext : DbContext
+    {
+        public WebApi_EFContext (DbContextOptions<WebApi_EFContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApi_EF.Models.Book> Book { get; set; }
+    }
+}
